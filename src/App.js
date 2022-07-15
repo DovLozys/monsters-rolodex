a460fe34675d0a001e5b2fa7ab99f5f16e215581
Dovydas Lozys <755086+reduktorius@users.noreply.github.com>
2022-07-15 09:41:18 +0100

    adds dummy data array, maps over it, displays name

--- src/App.js
+++ src/App.js
@@ -7,21 +7,29 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'redu',
-      company: 'Frageo'
+      monsters: [
+        {
+          name: 'Dov'
+        },
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Red'
+        },
+        {
+          name: 'Jackie'
+        },
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}, you're from {this.state.company}.</p>
-          <button onClick={() => {
-            this.setState({name: 'Dov'})
-          }}>Change Name</button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
