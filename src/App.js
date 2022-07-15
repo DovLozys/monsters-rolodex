6f4c12cd75fdda968f233df75d41b28b244f5539
Dovydas Lozys <755086+reduktorius@users.noreply.github.com>
2022-07-15 12:49:04 +0100

    adds `fetch` for getting user data from API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          id: '1oi32jh',
-          name: 'Dov'
-        },
-        {
-          id: 'a1i32jh',
-          name: 'Linda'
-        },
-        {
-          id: 'a1oi3jh',
-          name: 'Red'
-        },
-        {
-          id: 'a1oi32j',
-          name: 'Jackie'
-        },
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => 
+      this.setState(
+        () => {
+          return { monsters: users}
+        },
+        () => {
+          console.log(this.state)
+        }
+      ))
   }
 
   render() {
